e0c8075b4f95d18083c2c6a5793a3eee76c81edf
Vladislav Legavin <vldikk12@gmail.com>
2021-09-28 16:01:53 +0300

    first

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Blessme on 28.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
